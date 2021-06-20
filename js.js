8b791022b2e8b240f32172f60560ea5faf104ae8
Lucas Curty <74004642+LucasCurty@users.noreply.github.com>
2021-06-20 18:11:35 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+let img = document.getElementById('img')
+let res = document.getElementById('res')
+let aud = document.getElementById('aud')
+  res.style.fontSize ='3em'
+  res.style.margin = '0px'
+function leao(){
+    res.innerHTML = 'Leão'
+    img.src ="imagens/lion-real.jpg"
+    aud.src ="sons/lion.mp3"
+}
+function gato(){
+    res.innerHTML = 'Gato'
+    img.src ="imagens/cat-real.jpg"
+    aud.src ="sons/gato.mp3"
+}
+function cachorro(){
+    res.innerHTML = 'Cachorro'
+    img.src ="imagens/dog-real.jpg"
+    aud.src ="sons/cachorro.mp3"
+   }
+function macaco(){
+    res.innerHTML = 'Macaco'
+    img.src ="imagens/monkey-real.jpg"
+    aud.src ="sons/macaco.mp3"
+}
+      
+
+    
